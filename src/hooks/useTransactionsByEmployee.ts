9f7ad8a2b7e70b9abab089dff134268f0c180664
Sarang <46310322+i-supermario@users.noreply.github.com>
2023-10-15 15:48:34 -0700

    Initial commit

---
+++ src/hooks/useTransactionsByEmployee.ts
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react"
+import { RequestByEmployeeParams, Transaction } from "../utils/types"
+import { TransactionsByEmployeeResult } from "./types"
+import { useCustomFetch } from "./useCustomFetch"
+
+export function useTransactionsByEmployee(): TransactionsByEmployeeResult {
+  const { fetchWithCache, loading } = useCustomFetch()
+  const [transactionsByEmployee, setTransactionsByEmployee] = useState<Transaction[] | null>(null)
+
+  const fetchById = useCallback(
+    async (employeeId: string) => {
+      // console.log("calling fetchById")
+
+      if(employeeId.length===0){
+        return
+      }
+
+      const data = await fetchWithCache<Transaction[], RequestByEmployeeParams>(
+        "transactionsByEmployee",
+        {
+          employeeId,
+        }
+      )
+      // console.log(data)
+
+      setTransactionsByEmployee(data)
+    },
+    [fetchWithCache]
+  )
+
+  const invalidateData = useCallback(() => {
+    setTransactionsByEmployee(null)
+  }, [])
+
+  return { data: transactionsByEmployee, loading, fetchById, invalidateData }
+}
